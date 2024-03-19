be2a3314f5f23f34577f833f9807c005eb31e910
Shivam Mishra <108998384+shivamm-55@users.noreply.github.com>
2024-03-19 21:59:05 +0530

    Update Collections.jsx

--- src/page/home/Collections.jsx
+++ src/page/home/Collections.jsx
@@ -8,9 +8,7 @@ const Collections = () => {
         <div className="md:w-1/2">
           <img src="images/zara-logo.png" alt="" />
           <p className="text-lg text-white capitalize my-8 md:w-2/3 leading-[32px]">
-            Lustrous yet understated. The new evening wear collection
-            exclusively offered at the reopened Giorgio Armani boutique in Los
-            Angeles.
+            Chic and Elegant: Explore Zara's Latest Evening Wear Collection, Exclusively Available Now!
           </p>
           <button className="bg-white text-Black font-semibold px-6 py-2 capitalize rounded-sm hover:-translate-y-2 transition-all duration-300">
             See Collections
